90bef983e987c747a8d04bf5720d0ee8b7fc1874
supercool_5 <70787564+uttam-bhai@users.noreply.github.com>
2022-07-27 19:24:49 +0530

    Add files via upload

---
+++ t&t/package.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>package</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="index.php" class="logo">travel.</a>
+
+   <nav class="navbar">
+      <a href="index.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/header-bg-2.png) no-repeat">
+   <h1>packages</h1>
+</div>
+
+<!-- packages section starts  -->
+
+<section class="packages">
+
+   <h1 class="heading-title">top destinations</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-1.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Taj Mahal</h3>
+            <p>The Taj Mahal , is an Islamic ivory-white marble mausoleum on the right bank of the river
+               Yamuna in the Indian city of Agra. It was commissioned in 1632 by the Mughal emperor Shah
+               Jahan  to house the tomb of his favourite wife, Mumtaz Mahal; it also houses
+               the tomb of Shah Jahan</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-2.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Sea View</h3>
+            <p>The sea, connected as the world ocean or simply the ocean, is the body of salty water that covers
+               approximately 71 percent of the Earth's surface. The word sea is also used to denote second-order
+               sections of the sea, such as the Mediterranean Sea, as well as certain large, entirely landlocked,
+               saltwater lakes, such as the Caspian Sea.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-3.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3> Kapalua Bay</h3>
+            <p>The gorgeous Kapalua Bay is known for its long rocky stretches of beach on either side of the bay, 
+               giving it that extra safe and sheltered feeling. With white sand beaches and shallow waters which
+                are rich in coral, it is the perfect spot for swimming and snorkeling no matter the time of year.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-4.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3> The Forbidden City</h3>
+            <p>One of the top historic places in China, the Forbidden City housed China’s imperial family for five
+                centuries. It was constructed during the Ming Dynasty (1368-1644) and its reign as an imperial residence
+                 lasted until the Qing Dynasty (1644-1911). The Forbidden City has housed 24 emperors, including the last
+                  Chinese emperor, Emperor Puyi who lost his imperial title in 1924.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-5.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Hogenakkal Falls – Krishnagiri Dam</h3>
+            <p>For those living in either Bengaluru or Mysuru crossing the state border over a
+               weekend is nothing new. Near the border of Karnataka are two beautiful places of
+               the state of Tamil Nadu, namely Hogenakkal and Krishnagiri. Both these small 
+               villages are blessed with nature’s bounty.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-6.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Goa</h3>
+            <p>Goa is a state on the southwestern coast of India within the Konkan region, 
+               geographically separated from the Deccan highlands by the Western Ghats.It 
+               is located between the Indian states of Maharashtra to the north and Karnataka
+                to the east and south, with the Arabian Sea forming its western coast.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-7.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Marine Lines</h3>
+            <p>Marine Lines is a locality in South Mumbai. The name Marine Lines is derived from
+               the Marine Battalion Lines, a military establishment built by the British in the 
+               19th century. The battalion was later converted to an air force residential quarters, 
+               and now lies just south of Metro Adlabs</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-8.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Cadillac Mountain</h3>
+            <p>Want to catch the sunrise before anyone else in America does? The morning's first 
+               sunrise can be seen from the top of Cadillac Mountain from 5:00  5:30 AM in 
+               September / October time, depending on the day (you can find the exact times here). 
+               From the top of the mountain – more than 1,530 ft up – you’ll also get a stunning view 
+               of the Atlantic as well as vast, rugged coastline. Cadillac Mountain really is one of the 
+               best sunrise spots in America. It’s well worth a visit!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-9.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3> Dal Lake</h3>
+            <p>One of the most famous lakes in Kashmir, the Dal Lake is actually made of three lakes and doesn't 
+               look like a normal lake at all. This is due to its maze like waterways, channels, houseboats, and 
+               floating islands all making it look like a group of islands in a water body. This is one of the 
+               most famous attractions in Srinagar and a stay in a houseboat here, truly offers an awesome experience.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-10.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3> Thar Desert</h3>
+            <p>The Thar Desert, also known as the Great Indian Desert, is an arid region in the north-western part of the 
+               Indian subcontinent that covers an area of 200,000 km2 (77,000 sq mi) and forms a natural boundary between 
+               India and Pakistan. It is the world's 20th-largest desert, and the world's 9th-largest hot subtropical desert.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-11.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Marmara Sea</h3>
+            <p>Istanbul is surrounded by the Marmara Sea in the South and the Black Sea in the north. Bosphorus divides the city into two parts, European and Asian sides. Plus there is Golden horn which is the part of Bosphorus and it goes even deeper into the city.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-12.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Luxury Resorts around Mumbai</h3>
+            <p>Monteria Resort, Upper Deck Resort, Soma Vine Village, Prakruti resort, Della Adventure Resorts, Zara Resorts, Dukes Resorts, The Machan Resort and various others in the proximity of Mumbai.</p>
+
+            <p>Boasting with a wide array of Luxury resorts near Mumbai these retreats draw visitors from both near and far. </p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"><span class="btn">load more</span></div>
+
+</section>
+
+<!-- packages section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="index.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +91 7710843691 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +91 9670064455 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> supercool@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> mumbai, india - 421201 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>supercool_5 web developer</span> | all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="script.js"></script>
+
+</body>
+</html>
